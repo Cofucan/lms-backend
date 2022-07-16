7054765045fa47b0873b74c3785ac67f32151ae4
josephhensh <josephhensh2000@gmail.com>
2022-07-16 12:44:24 +0100

    Created submit-task endpoint

--- lms/routers/task.py
+++ lms/routers/task.py
@@ -7,3 +7,13 @@ from fastapi import (
 
 
 router = APIRouter(prefix="/task")
+
+@router.post(
+    "/submit-task/",
+    name="task:submit",
+    status_code=status.HTTP_200_OK,
+    response_model=TaskPublic,
+    description="Submit a task."    
+)
+async def submit_task():
+    pass 
