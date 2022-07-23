ae91c5c110e454ef44218656cabfef108239707d
Cofucan <coolucheofia@gmail.com>
2022-07-23 01:46:52 +0100

    added unit test for: general announcement creation

--- lms/tests/test_dashboard.py
+++ lms/tests/test_dashboard.py
@@ -9,9 +9,9 @@ from models.user import User
 
 from essential_generators import DocumentGenerator
 from library.dependencies.test_data import (
-    generate_user, 
-    generate_lesson, 
-    generate_announcement
+    generate_user,
+    generate_lesson,
+    generate_announcement,
 )
 
 gen = DocumentGenerator()
@@ -19,22 +19,35 @@ pytestmark = pytest.mark.asyncio
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-# class TestAnnouncement:
-#     user = generate_user()
-#     announcement = generate_announcement(False)
+class TestAnnouncement:
+    user = generate_user()
+    announcement = generate_announcement(True)
 
-#     # Test general announcement creation
-#     async def test_announcement_general(
-#         self, app: FastAPI, client: AsyncClient, test_user
-#     ) -> None:
+    # Test general announcement creation
+    async def test_announcement_general(
+        self, app: FastAPI, client: AsyncClient, test_user
+    ) -> None:
+
+        # Login admin
+        admin_data = {
+            "email": test_user.email,
+            "password": "@123Qwerty",
+        }
+        response = await client.post(
+            app.url_path_for("auth:login"), json=admin_data
+        )
+        assert response.status_code == 200
+
+        admin_token = response.json().get("token")
+
+        # Create announcement
+        response = await client.post(
+            app.url_path_for("announcements"),
+            json=self.announcement,
+            headers={"Authorization": f"Bearer {admin_token}"},
+        )
+        assert response.status_code == 201
 
 
-
-
-
-#         # Create announcement
-#         response = await client.post(
-#             app.url_path_for("dashboard:announcement"), json=self.announcement
-#         )
-#         assert response.status_code == 201
-
+# Test unauthorized
+# Test user announcement
