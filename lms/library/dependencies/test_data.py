a5b58133c86d2a5eb951912c476d2edf10e7e06b
Cofucan <coolucheofia@gmail.com>
2022-07-23 01:43:52 +0100

    adjusted template data

--- lms/library/dependencies/test_data.py
+++ lms/library/dependencies/test_data.py
@@ -6,45 +6,79 @@ gen = DocumentGenerator()
 
 def generate_user():
     template = {
-        'first_name': 'name',
-        'surname': 'name',
-        'email': 'email',
-        'password': 'bill#oJworl765d',
+        "first_name": "name",
+        "surname": "name",
+        "email": "email",
+        "password": "bill#oJworl765d",
     }
     gen.set_template(template)
     return gen.document()
 
 
 def generate_announcement(general: bool):
-    template_general = {
-        'title': 'name',
-        'content': 'sentence',
-        'general': 'true',
-        'stage': random.randint(1, 20),
-    }
-    template_target = {
-        'title': 'sentence',
-        'content': 'paragraph',
-        'stack': ["backend", "frontend", "design", "product management", "cloud engineering", "data science"],
-        'track': ["nodejs", "python", "php", "golang", "vuejs", "reactjs", "vanillajs", "product design", "ui/ux design"],
-        'proficiency': ["beginner", "intermediate", "advanced"],
-        'stage': random.randint(1, 20),
+    template_user = {
+        "title": "name",
+        "content": "sentence",
+        "stack": [
+            "backend",
+            "frontend",
+            "design",
+            "product management",
+            "cloud engineering",
+            "data science",
+        ],
+        "track": [
+            "nodejs",
+            "python",
+            "php",
+            "golang",
+            "vuejs",
+            "reactjs",
+            "vanillajs",
+            "product design",
+            "ui/ux design",
+        ],
+        "proficiency": ["beginner", "intermediate", "advanced"],
+        "stage": random.randint(1, 20),
+        "general": "false",
     }
     if general:
+        template_general = {
+            "title": "name",
+            "content": "sentence",
+            "general": "true",
+        }
         gen.set_template(template_general)
     else:
-        gen.set_template(template_target)
+        gen.set_template(template_user)
     return gen.document()
 
 
 def generate_lesson():
     template = {
-        'title': 'sentence',
-        'content': 'paragraph',
-        'stack': ["backend", "frontend", "design", "product management", "cloud engineering", "data science"],
-        'track': ["nodejs", "python", "php", "golang", "vuejs", "reactjs", "vanillajs", "product design", "ui/ux design"],
-        'proficiency': ["beginner", "intermediate", "advanced"],
-        'stage': random.randint(1, 20),
+        "title": "sentence",
+        "content": "paragraph",
+        "stack": [
+            "backend",
+            "frontend",
+            "design",
+            "product management",
+            "cloud engineering",
+            "data science",
+        ],
+        "track": [
+            "nodejs",
+            "python",
+            "php",
+            "golang",
+            "vuejs",
+            "reactjs",
+            "vanillajs",
+            "product design",
+            "ui/ux design",
+        ],
+        "proficiency": ["beginner", "intermediate", "advanced"],
+        "stage": random.randint(1, 20),
     }
     gen.set_template(template)
     return gen.document()
@@ -52,15 +86,32 @@ def generate_lesson():
 
 def generate_task():
     template = {
-        'title': 'sentence',
-        'content': 'paragraph',
-        'stack': ["backend", "frontend", "design", "product management", "cloud engineering", "data science"],
-        'track': ["nodejs", "python", "php", "golang", "vuejs", "reactjs", "vanillajs", "product design", "ui/ux design"],
-        'proficiency': ["beginner", "intermediate", "advanced"],
-        'stage': random.randint(1, 20),
-        'feedback': 'sentence',
-        'active': 'true',
-        'deadline': random.randint(1, 14),
+        "title": "sentence",
+        "content": "paragraph",
+        "stack": [
+            "backend",
+            "frontend",
+            "design",
+            "product management",
+            "cloud engineering",
+            "data science",
+        ],
+        "track": [
+            "nodejs",
+            "python",
+            "php",
+            "golang",
+            "vuejs",
+            "reactjs",
+            "vanillajs",
+            "product design",
+            "ui/ux design",
+        ],
+        "proficiency": ["beginner", "intermediate", "advanced"],
+        "stage": random.randint(1, 20),
+        "feedback": "sentence",
+        "active": "true",
+        "deadline": random.randint(1, 14),
     }
     gen.set_template(template)
     return gen.document()
