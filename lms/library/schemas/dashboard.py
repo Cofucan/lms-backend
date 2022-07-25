82eb74c043e811320ae3a557421ae79ae1593507
josephhensh <josephhensh2000@gmail.com>
2022-07-25 11:41:31 +0100

    Modify dashboard schema, dashboard route and deleted task.py from routers

--- lms/library/schemas/dashboard.py
+++ lms/library/schemas/dashboard.py
@@ -6,6 +6,7 @@ from library.dependencies.utils import regex, validate_stack_and_track
 from library.schemas.register import UserPublic
 from library.schemas.common import CommonBase, CommonResponse
 from library.schemas.enums import Stack, Track, Proficiency
+from library.schemas.shared import SharedModel
 
 
 class AnnouncementCreate(BaseModel):
@@ -120,7 +121,7 @@ class ResourceCreate(CommonBase):
 
 class ResourceResponse(BaseModel):
     creator: UserPublic
-    resources: ResourceSchema
+    resources: ResourceCreate
 
 
 class TaskSubmissionSchema(BaseModel):
@@ -132,4 +133,4 @@ class TaskPublicSchema(SharedModel):
     url: str
     graded: bool
     submitted: bool
-    resources: ResourceCreate
+    
