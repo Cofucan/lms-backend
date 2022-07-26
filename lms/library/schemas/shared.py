e7729a8e4b715106512ccb4f29cfe9394a038628
mykie88 <mykhelmyers@gmail.com>
2022-07-26 13:30:51 +0100

    code refactor, separated endpoints into modules

--- lms/library/schemas/shared.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from uuid import UUID
-from datetime import datetime
-
-
-class SharedModel(BaseModel):
-    id: UUID
-    created_at: datetime
-    updated_at: datetime | None
--- lms/models/user.py
+++ lms/models/user.py
@@ -3,16 +3,16 @@ from models.base import BaseModel
 
 
 class User(BaseModel):
-    username = fields.CharField(max_length=50, null=True)
+    username = fields.CharField(max_length=55, null=True)
     first_name = fields.CharField(max_length=100, null=True)
     surname = fields.CharField(max_length=100, null=True)
     email = fields.CharField(max_length=100, null=True)
-    phone = fields.CharField(max_length=100, null=True)
-    gender = fields.CharField(max_length=100, null=True)
+    phone = fields.CharField(max_length=55, null=True)
+    gender = fields.CharField(max_length=55, null=True)
     stage = fields.IntField(null=True)
-    stack = fields.CharField(max_length=100, null=True)
-    track = fields.CharField(max_length=100, null=True)
-    proficiency = fields.CharField(max_length=100, null=True)
+    stack = fields.CharField(max_length=55, null=True)
+    track = fields.CharField(max_length=55, null=True)
+    proficiency = fields.CharField(max_length=55, null=True)
     last_active = fields.DatetimeField(auto_now=True)
     is_admin = fields.BooleanField(default=False)
     hashed_password = fields.CharField(max_length=255, null=True)
